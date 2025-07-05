f585737100b60c3560d54df989a39df4c414875e
Noah Gardner <ngngardner@gmail.com>
2025-07-05 17:10:35 -0400

    feat: added processing time tracking

--- src/viroc/app.py
+++ src/viroc/app.py
@@ -1,4 +1,5 @@
 import io
+import time
 
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
@@ -14,18 +15,25 @@ ocr_model = GOTOCRModel()
 
 @app.post("/predict")
 async def predict_image(file: UploadFile = File(...)) -> JSONResponse:
+    tick = time.time()
     # Read image file
     image_data = await file.read()
     image = Image.open(io.BytesIO(image_data))
 
     # Make prediction
     bb = detect_model.get_bounding_box(image)
+    detection_time = time.time() - tick
     plate = extract_bounding_box(image.copy(), bb)
     generated_text = ocr_model.predict(plate)
+    ocr_time = time.time() - tick - detection_time
 
     # Return response
+    tock = time.time()
     return JSONResponse(
         {
             "license_plates": [generated_text],
+            "processing_time_ms": f"{(tock - tick) * 1000:.2f}ms",
+            "detection_time_ms": f"{detection_time * 1000:.2f}ms",
+            "ocr_time_ms": f"{ocr_time * 1000:.2f}ms",
         }
     )
